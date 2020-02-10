af28b15f78b6059e97bbe4ca4c8294d99efe919b
Nicholas Sandomeno <nicholassandomeno@Nicholass-Air.home>
2020-02-10 17:16:14 -0500

    Post request of a new user complete.

--- index.js
+++ index.js
@@ -16,6 +16,22 @@ server.get('/', (req, res) => {
     res.json({ greeting: 'Hello'})
 })
 
+// (1) post request - add a user
+server.post('/api/users', (req, res) => {
+    const newUser = req.body
+
+    if (newUser.name && newUser.bio) {
+        Users.insert(newUser).then((user) => {
+            console.log("This is the response from new user post:", user)
+            res.status(201).json(user)
+        }).catch((error) => {
+            res.status(400).json({ error_message: "Please provide a name and a bio for the new user."})
+        })
+    } else {
+        res.status(500).json({ error_message: "There was an error while saving the user to the database."})
+    }
+})
+
 // (2) get request - all users
 server.get('/api/users', (req, res) => {
     Users.find().then((users) => {
@@ -25,6 +41,8 @@ server.get('/api/users', (req, res) => {
         res.status(500).json({ error_message: "The User information could not be retrieved."})
     })
 })
+
+
 
 
 
